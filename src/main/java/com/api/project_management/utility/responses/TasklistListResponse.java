001f90d0c8941aeb1a94b158a73a6c2dadb6bbce
iliask796 <iliaskal7@gmail.com>
2023-10-12 16:37:10 +0300

    Responses: Tasklist Response + List

---
+++ src/main/java/com/api/project_management/utility/responses/TasklistListResponse.java
@@ -0,0 +1,9 @@
+package com.api.project_management.utility.responses;
+
+import com.api.project_management.model.Tasklist;
+import com.api.project_management.utility.DataResponse;
+
+import java.util.List;
+
+public class TasklistListResponse extends DataResponse<List<Tasklist>> {
+}
---
+++ src/main/java/com/api/project_management/utility/responses/TasklistResponse.java
@@ -0,0 +1,7 @@
+package com.api.project_management.utility.responses;
+
+import com.api.project_management.model.Tasklist;
+import com.api.project_management.utility.DataResponse;
+
+public class TasklistResponse extends DataResponse<Tasklist> {
+}
