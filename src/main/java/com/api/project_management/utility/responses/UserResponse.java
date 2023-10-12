6ac03e79e976b82d90f6220a1c1d9512f801c4b3
iliask796 <iliaskal7@gmail.com>
2023-10-12 11:55:05 +0300

    Responses: User Response

---
+++ src/main/java/com/api/project_management/utility/responses/UserResponse.java
@@ -0,0 +1,7 @@
+package com.api.project_management.utility.responses;
+
+import com.api.project_management.model.User;
+import com.api.project_management.utility.DataResponse;
+
+public class UserResponse extends DataResponse<User> {
+}
