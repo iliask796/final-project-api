8a551f3ce57a0be41d10ca02b80170d1334281f9
iliask796 <iliaskal7@gmail.com>
2023-10-12 16:19:20 +0300

    Responses: Workspace Response

---
+++ src/main/java/com/api/project_management/utility/responses/WorkspaceResponse.java
@@ -0,0 +1,7 @@
+package com.api.project_management.utility.responses;
+
+import com.api.project_management.model.Workspace;
+import com.api.project_management.utility.DataResponse;
+
+public class WorkspaceResponse extends DataResponse<Workspace> {
+}
