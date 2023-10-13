a770b304dce13acea0b790232e9862d1dc26fe22
iliask796 <iliaskal7@gmail.com>
2023-10-13 13:15:28 +0300

    Responses: Task Responses

---
+++ src/main/java/com/api/project_management/utility/responses/ListTaskResponse.java
@@ -0,0 +1,9 @@
+package com.api.project_management.utility.responses;
+
+import com.api.project_management.model.Task;
+import com.api.project_management.utility.DataResponse;
+
+import java.util.List;
+
+public class ListTaskResponse extends DataResponse<List<Task>> {
+}
---
+++ src/main/java/com/api/project_management/utility/responses/TaskResponse.java
@@ -0,0 +1,7 @@
+package com.api.project_management.utility.responses;
+
+import com.api.project_management.model.Task;
+import com.api.project_management.utility.DataResponse;
+
+public class TaskResponse extends DataResponse<Task> {
+}
