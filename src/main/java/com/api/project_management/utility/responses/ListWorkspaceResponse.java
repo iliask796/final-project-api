5b755235a12ab698d3a8ed9a0dfb8310eed4649c
iliask796 <iliaskal7@gmail.com>
2023-10-26 16:31:30 +0300

    Responses: ListWorkspace Response

---
+++ src/main/java/com/api/project_management/utility/responses/ListWorkspaceResponse.java
@@ -0,0 +1,9 @@
+package com.api.project_management.utility.responses;
+
+import com.api.project_management.model.Workspace;
+import com.api.project_management.utility.DataResponse;
+
+import java.util.List;
+
+public class ListWorkspaceResponse extends DataResponse<List<Workspace>> {
+}
